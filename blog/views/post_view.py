bcc57d688100538b473455fff6f2507ee44272c8
unknown <mauro.ibsen@gmail.com>
2022-11-11 11:11:16 -0300

    modulo8

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.views import generic
 
-
 class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return None # HttpResponse('Hello World')
+    def get(self,request,*args,**kwargs):
+        return HttpResponse('Hello World')
--- tests/views/test_post.py
+++ tests/views/test_post.py
@@ -1,11 +1,16 @@
-import pytest
+#import json
 
+import pytest
 from django.urls import reverse
 
-
 @pytest.mark.django_db
-def test_post_view(client):
-    url = reverse('home')
-    response = client.get('url')
-    assert response.status_code == 200
 
+def test_post_view(client):
+    url=reverse('home')
+    response=client.get(url)
+    assert response.status_code==200
+    # import pdb
+    # pdb.set_trace()
+    # response=json.loads(response.content)
+
+    assert response.content == b'Hello World'
